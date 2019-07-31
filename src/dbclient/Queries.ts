98c791df4eaef355e22da4412b23d6ea7d7feb21
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-07-31 13:48:27 +0530

    update column name

--- src/dbclient/Queries.ts
+++ src/dbclient/Queries.ts
@@ -1,3 +1,3 @@
 
-export const SELECT_FACES_BY_NAME = 'SELECT personId,embedding,name FROM face WHERE name = $1 LIMIT $2',
+export const SELECT_FACES_BY_NAME = 'SELECT personId,embedding,personName FROM face WHERE personName = $1 LIMIT $2',
     INSERT_FACE_DATA = 'INSERT INTO face VALUES($1, $2, $3, $4)'
