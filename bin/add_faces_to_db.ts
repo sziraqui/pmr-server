f73d6fe0151981a71e05384e507781fc180f9b84
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-08-01 19:15:35 +0530

    fix db insertion line not executing

--- bin/add_faces_to_db.ts
+++ bin/add_faces_to_db.ts
@@ -1,46 +1,47 @@
 #!/usr/bin/env ts-node
 import { ArgumentParser } from 'argparse';
-import { readdir } from 'fs';
+import { readdir, readdirSync } from 'fs';
 import path from 'path';
-import { readImage } from 'nodoface';
-import { Facenet, FaceDetectorMTCNN, FaceDetector, utils } from '../../pmr-core/src/index';
+import { readImage, showImage, waitKey } from 'nodoface';
+import { Facenet, FaceDetector, FaceModelParameters } from '../../pmr-core/src/index';
 import { db } from '../';
-import { Tensor3D } from '@tensorflow/tfjs-node';
+import { DbHelper } from '../src/dbclient';
 
 let parser = new ArgumentParser();
 parser.addArgument('dataPath', { help: 'dataset directory' });
-
+let faceModelParams = new FaceModelParameters([__dirname]);
+let mtcnn: FaceDetector, facenet: Facenet, dbh: DbHelper;
 let args = parser.parseArgs();
 
 async function run(dataPath: string) {
-    readdir(dataPath, (err, dirs) => {
-        if (err) throw err;
-        dirs.forEach((subdir, i) => {
-            // handle face dir
-            handleFaceDir(path.join(dataPath, subdir));
-        });
-    });
+    mtcnn = await FaceDetector.getInstance({ detector: 'mtcnn', weightsPath: faceModelParams.getMtcnnLocation() });
+    facenet = await Facenet.getInstance();
+    dbh = new db.DbHelper();
+    let dirs = readdirSync(dataPath);
+    for (let subdir of dirs) {
+        console.log(subdir);
+        await handleFaceDir(path.join(dataPath, subdir));
+    }
 }
 
 async function handleFaceDir(dir: string) {
-    readdir(dir, (err, files) => {
-        if (err) console.log(`Err:${err.message}`);
-        files.forEach(async (file, i) => {
-            // add face to db
-            let img = readImage(path.join(dir, file));
-            let mtcnn = await FaceDetector.getInstance({ detector: 'mtcnn' });
-            let facenet = await Facenet.getInstance();
-            let faceBlobs = await mtcnn.detect(img);
-            faceBlobs.forEach(async (faceBlob) => {
-                faceBlob.descriptor = await facenet.embedding(faceBlob.faceImage);
-                console.log(faceBlob.descriptor.length);
-                let personName = path.basename(dir).replace('_', ' ');
-                let dbh = new db.DbHelper();
-                dbh.insertFace(Array.from(faceBlob.descriptor), personName);
-                // process.exit(0);
-            });
-        });
-    });
+    let files = readdirSync(dir);
+    for (let file of files) {
+        // add face to db
+        let img = readImage(path.join(dir, file));
+        let faceBlobs = await mtcnn.detect(img);
+        console.log('Detected faces', faceBlobs.length);
+        for (let faceBlob of faceBlobs) {
+            faceBlob.descriptor = await facenet.embedding(faceBlob.faceImage);
+            let personName = path.basename(dir).replace('_', ' ');
+            console.log(file)
+            showImage(img.extract(faceBlob.bbox), file);
+            waitKey(0);
+            let res = await dbh.insertFace(Array.from(faceBlob.descriptor), personName);
+            console.log('inserted', personName);
+        }
+    }
 }
+
 console.log(args);
 run(path.resolve(args.dataPath));
