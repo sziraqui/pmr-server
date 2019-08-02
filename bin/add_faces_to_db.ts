47b9b85e92419512bef32baff8a955b87c221245
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-08-02 13:55:40 +0530

    fix unhandled promise

--- bin/add_faces_to_db.ts
+++ bin/add_faces_to_db.ts
@@ -17,15 +17,23 @@ let faceModelParams = new FaceModelParameters([__dirname]);
 let mtcnn: FaceDetector, facenet: Facenet, dbh: DbHelper;
 
 async function run(dataPath: string) {
-    mtcnn = await FaceDetector.getInstance({ detector: 'mtcnn', weightsPath: faceModelParams.getMtcnnLocation() });
-    facenet = await Facenet.getInstance();
-    dbh = new db.DbHelper();
+    try {
+        mtcnn = await FaceDetector.getInstance({ detector: 'mtcnn', weightsPath: faceModelParams.getMtcnnLocation() });
+        facenet = await Facenet.getInstance();
+        dbh = new db.DbHelper();
+    } catch (err) {
+        return Promise.reject(err.message);
+    }
     readdir(dataPath, async (err, dirs) => {
         if (err) throw err;
         dirs.forEach(async (subdir, i) => {
             // handle face dir
             console.log(`Processing ${i} of ${dirs.length}`);
-            await handleFaceDir(path.join(dataPath, subdir));
+            try {
+                await handleFaceDir(path.join(dataPath, subdir));
+            } catch (error) {
+                return Promise.reject(error);
+            }
         });
     });
 }
@@ -35,17 +43,26 @@ async function handleFaceDir(dir: string) {
         if (err) console.log(`Err:${err.message}`);
         files.forEach(async (file) => {
             // add face to db
-            let img = readImage(path.join(dir, file));
-            let faceBlobs = await mtcnn.detect(img);
-            faceBlobs.forEach(async (faceBlob) => {
-                faceBlob.descriptor = await facenet.embedding(faceBlob.faceImage);
-                let personName = path.basename(dir).replace('_', ' ');
-                let res = await dbh.insertFace(Array.from(faceBlob.descriptor), personName);
-                res['update_dataset_status'] = await dbh.updateDatasetInfo(args.dataset.toUpperCase(), args.variant.toUpperCase(), path.join(path.basename(dir), file), res['face_id']);
-                return res;
-            });
+            try {
+                let img = readImage(path.join(dir, file));
+                let faceBlobs = await mtcnn.detect(img);
+                faceBlobs.forEach(async (faceBlob) => {
+                    try {
+                        faceBlob.descriptor = await facenet.embedding(faceBlob.faceImage);
+                        let personName = path.basename(dir).replace('_', ' ');
+                        let res = await dbh.insertFace(Array.from(faceBlob.descriptor), personName);
+                        res['update_dataset_status'] = await dbh.updateDatasetInfo(args.dataset.toUpperCase(), args.variant.toUpperCase(), path.join(path.basename(dir), file), res['face_id']);
+                        return res;
+                    } catch (err1) {
+                        return Promise.reject(err.message);
+                    }
+                });
+            } catch (error) {
+                return Promise.reject(error);
+            }
         });
     });
 }
 
-run(path.resolve(args.dataPath));
+run(path.resolve(args.dataPath))
+    .catch(err => console.log(err));
